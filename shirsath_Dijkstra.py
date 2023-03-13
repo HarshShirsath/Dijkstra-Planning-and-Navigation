c33a763c8d478f34d5331e5b210203ac4a293ea7
HarshShirsath <harshshirsath98@gmail.com>
2023-03-12 23:29:22 -0400

    Adding CV2 frames

--- shirsath_Dijkstra.py
+++ shirsath_Dijkstra.py
@@ -84,3 +84,50 @@ def plot(surface,start,goal_path):
             node_new = (x,y)
             return (surface,node_new,cost)
         return (surface,None, None)
+    
+    def northwest(surface, x_ynode, cost):
+        x,y = x_ynode
+        if x > 0 and y < surface.shape[0]-1 :
+            x -= 1
+            y += 1
+            cost += 1.4
+            node_new = (x,y)
+            return (surface,node_new,cost)
+        return (surface,None,None)
+    
+    
+    
+    def to_be_visited(surface,x_ynode):
+        parent = x_ynode
+        peak_cost = nodes[x_ynode][0]
+        for i in [north,northeast,east,southeast,south,southwest,west,northwest]:
+            surface,node_new,cost = i(surface,x_ynode, peak_cost)
+            if node_new == None:
+                continue
+            (x,y)  = node_new
+            y_up = (surface.shape[0]-1)-y
+            if node_new in close_loop:
+                continue
+            layer = visual(surface,node_new)
+            if np.any(layer[:,:,1]):
+                continue
+
+            if node_new not in nodes or cost < nodes[node_new][0]:
+                global index
+                index += 1
+                nodes[(x,y)] = (cost, index, parent)
+                
+                hq.heappush(open_loop, (cost,node_new))
+            if node_new == goal_path:
+                break 
+
+
+    def visual(frame,x_ynode):
+        x,y = x_ynode
+        r_circle = 5
+        layer = np.zeros_like(frame)
+        cv2.circle(layer, (x, y), r_circle, (255,255,255), -1)
+        mask_circle = cv2.bitwise_and(frame, layer)
+        
+        return mask_circle
+        
